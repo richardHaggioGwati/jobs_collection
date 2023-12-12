123c2d51e4ec5cd3238a21d996449d68f122fe5a
richardHaggioGwati <haggiog0844@gmail.com>
2023-12-12 17:13:19 +0200

    dynamic route ui

--- app/job-details/[id].js
+++ app/job-details/[id].js
@@ -1,8 +1,70 @@
-import { Text } from 'react-native'
+import {ActivityIndicator, RefreshControl, SafeAreaView, ScrollView, View} from 'react-native'
+import {useRouter, useLocalSearchParams, Stack} from "expo-router";
+import {useState} from "react";
+import {JobTabs} from "../../components";
+import useFetch from "../../hook/useFetch";
+import {COLORS, SIZES} from "../../constants";
+import icons from "../../constants/icons";
+
+import Company from "../../components/jobdetails/company/Company";
+import ScreenHeaderBtn from "../../components/common/header/ScreenHeaderBtn";
 
 const JobDetail = () => {
+    const tabs = ['About', 'Qualification', 'Responsibilities']
+
+    const params = useLocalSearchParams()
+    const router = useRouter()
+    const [refreshing, SetRefreshing] = useState(false)
+    const [activeTab, setActiveTab] = useState(tabs[0])
+
+    const {data, loading, refetch} = useFetch('job-details', {
+        job_id: params.id
+    })
+
+    const displayTabContent = () => {
+        switch (activeTab) {
+            case 'Qualifications':
+            case 'Responsibilities':
+            case 'About':
+            default:
+                break;
+        }
+    }
+
+    const onRefreshing = () => {}
+
     return (
-        <Text>Job Detail</Text>
+        <SafeAreaView style={{ flex: 1, backgroundColor: COLORS.lightWhite }}>
+            <Stack.Screen options={{
+                headerStyle: {backgroundColor: COLORS.lightWhite},
+                headerShadowVisible: false,
+                headerBackVisible: false,
+                headerLeft: () => (
+                    <ScreenHeaderBtn iconUrl={icons.left} dimensions="60%" handlePress={() => router.back()}/>
+                ),
+                //TODO: implement share
+                headerRight: () => (
+                    <ScreenHeaderBtn iconUrl={icons.share} dimensions="60%"/>
+                ),
+                headerTitle: '',
+            }}
+            />
+
+            <>
+                <ScrollView showsVerticalScrollIndicator={false} refreshControl={<RefreshControl refreshing={refreshing} onRefresh={onRefreshing}/> }>
+                    {loading || !data ? (
+                        <ActivityIndicator size="large" color={COLORS.primary} />
+                    ) : (
+                        <View style={{ padding: SIZES.medium, paddingBottom: 100}}>
+                            <Company companyData={data[0]}/>
+
+                            <JobTabs tabs={tabs} activeTab={activeTab} setActiveTab={setActiveTab} />
+                        </View>
+                    )}
+                    {displayTabContent}
+                </ScrollView>
+            </>
+        </SafeAreaView>
     )
 }
 
--- components/jobdetails/company/Company.jsx
+++ components/jobdetails/company/Company.jsx
@@ -1,14 +1,33 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import {View, Text, Image} from 'react-native'
 
 import styles from './company.style'
+import icons from "../../../constants/icons";
 
-const Company = () => {
-  return (
-    <View>
-      <Text>Company</Text>
-    </View>
-  )
+const Company = ({ companyData = {} }) => {
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.logoBox}>
+                {
+                    companyData.employer_logo ? <Image source={{uri: companyData.employer_logo}} style={styles.logoImage} /> :
+                        <Image source={require('../../../assets/images/job-post.jpg')} style={styles.logoImage} />
+                }
+            </View>
+
+            <View style={styles.jobTitleBox}>
+                <Text style={styles.jobTitle}>{companyData.job_title}</Text>
+            </View>
+
+            <View style={styles.companyInfoBox}>
+                <Text style={styles.companyName}>{companyData.employer_name} / </Text>
+                <View style={styles.locationBox}>
+                    <Image source={icons.location} resizeMode='contain' style={styles.locationImage}/>
+                    <Text style={styles.locationName}>{companyData.job_country}</Text>
+                </View>
+            </View>
+        </View>
+    )
 }
 
 export default Company
